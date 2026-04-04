0a372584d90ae5712e43c940f4fc5c06c5820245
Hardik <hardik7361@gmail.com>
2026-04-05 00:11:36 +0530

    fix 2

--- frontend/src/components/AICopilot.jsx
+++ frontend/src/components/AICopilot.jsx
@@ -62,7 +62,7 @@ const AICopilot = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch(`${API_BASE}/chat` {
+      const response = await fetch(`${API_BASE}/chat`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: input }),
